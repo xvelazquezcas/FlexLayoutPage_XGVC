a06e99ed243f3da7c5884b7e871dd814d3b8887c
velazquezxana597@gmail.com <velazquezxana597@gmail.com>
2025-02-19 22:28:00 -0700

    .

---
+++ AbsoluteLayout.xaml.cs
@@ -0,0 +1,9 @@
+namespace FlexLayoutPage_XGVC;
+
+public partial class AbsoluteLayout : ContentPage
+{
+	public AbsoluteLayout()
+	{
+		InitializeComponent();
+	}
+}
--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new FlexLayoutPage();
+            MainPage = new AbsoluteLayout();
         }
     }
 }
